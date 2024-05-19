74d78c38d9388972110d3c03ce80ef1abacf712f
PolyMath-Coder <you@example.com>
2024-05-19 15:23:28 +0100

    chore: implement database bootstrap

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
 
+config()
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TypeOrmModule.forRoot({type: 'mongodb', url: process.env.DB_URL})],
   controllers: [AppController],
   providers: [AppService],
 })
