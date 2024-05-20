ab34b5426755b335ef32ed563b0280892c43eacc
PolyMath-Coder <you@example.com>
2024-05-20 07:53:41 +0100

    chore: final commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalRoute } from './shared/constants';
-
+import { config } from 'dotenv';
+config()
 async function bootstrap() {
  
   const app = await NestFactory.create(AppModule);
   
   app.setGlobalPrefix(GlobalRoute.PREFIX)
-  await app.listen(3000);
+  await app.listen(process.env.PORT, () => {
+    console.log(`Server now live at port ${process.env.PORT}`)
+  });
 }
 bootstrap();
